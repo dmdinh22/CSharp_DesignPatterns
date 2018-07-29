6a4d6b69c2df2a3fb187caa532132f9a8a703ab7
David Dinh <dmdinh22@gmail.com>
2018-07-29 06:42:38 -0700

    bad overloaded methods

--- FactoryPattern/SimpleImmutability.cs
+++ FactoryPattern/SimpleImmutability.cs
@@ -2,18 +2,38 @@ using NUnit.Framework;
 
 namespace CSharp_DesignPatterns.FactoryPattern
 {
-    public class Point
+    // public class Point
+    // {
+    //     private readonly int x;
+    //     private readonly int y;
+
+    //     public int X { get { return x; } }
+    //     public int Y { get { return y; } }
+
+    //     public Point( int x, int y)
+    //     {
+    //         this.x = x;
+    //         this.y = y;
+    //     }
+    // }
+
+    public struct BadDuration
     {
-        private readonly int x;
-        private readonly int y;
+        private readonly long ticks;
 
-        public int X { get { return x; } }
-        public int Y { get { return y; } }
-
-        public Point( int x, int y)
+        public BadDuration(long ticks)
         {
-            this.x = x;
-            this.y = y;
+            this.ticks = ticks;
+        }
+
+        public BadDuration(long milliseconds)
+        {
+            this.ticks = milliseconds * 10000;
+        }
+
+        public BadDuration(long seconds)
+        {
+            this.ticks = seconds * 10000 * 1000;
         }
     }
 
