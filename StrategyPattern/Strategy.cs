f328b57643f7c80b69bdc3c85b78175f6b362cfd
David Dinh <dmdinh22@gmail.com>
2018-08-05 13:32:13 -0700

    Single Responsibilities with Strat Pattern

--- StrategyPattern/Strategy.cs
+++ StrategyPattern/Strategy.cs
@@ -1,3 +1,6 @@
+using System;
+using NUnit.Framework;
+
 namespace CSharp_DesignPatterns.StrategyPattern
 {
     public class Strategy
@@ -10,13 +13,54 @@ namespace CSharp_DesignPatterns.StrategyPattern
                 new Person { Name = "Jon", Age = 35 },
                 new Person { Name = "Rob", Age = 44 },
                 new Person { Name = "Holly", Age = 36 },
+                new Person { Name = "Jon", Age = 5 },
                 new Person { Name = "Tom", Age = 8 },
             }.AsReadOnly();
 
             [Test]
             public void SortingByAge()
             {
+                var list = People.ToList();
+                list.Sort(CompareByAge);
+                list.Sort((x, y) => x.Age.CompareTo(y.Age));
+                list.Sort(new AgeComparer());
 
+                foreach (var person in list)
+                {
+                    Console.WriteLine(person);
+                }
+            }
+
+            public class JonList<T>
+            {
+                public void Sort(Comparison<T> comparison)
+                {
+
+                }
+
+                public void Sort(IComparer<T> comparison)
+                {
+
+                }
+            }
+
+            public delegate int Comparison<in T>(T x, T y);
+            public interface IComparer<in T>
+            {
+                int Compare(T x, T y);
+            }
+
+            static int CompareByAge(Person x, Person y)
+            {
+                return x.Age.CompareTo(y.Age);
+            }
+
+            public class AgeComparer : IComparer<Person>
+            {
+                public int Compare(Person x, Person y)
+                {
+                    return x.Age.CompareTo(y.Age);
+                }
             }
         }
     }
